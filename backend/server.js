eb3d86d0ef8a9b34a3c488d1a62ee83da5db4a2b
Lucas Mosimann <votre.email@example.com>
2026-04-07 16:17:47 +0200

    (feat):Rajout des routes

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,28 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
+const cors = require('cors');
 const port = 3000;
+const router = express.Router();
 
+app.use(cors());
 app.use(express.json());
+const {
+  getTodos,
+  createTodo,
+  updateTodo,
+  deleteTodo,
+} = require('./controllers/todoController');
+
+
+router.get('/', getTodos);
+router.post('/', createTodo);
+router.put('/:id', updateTodo);
+router.delete('/:id', deleteTodo);
+
+app.use('/todos', router);
+
+
 
 mongoose.connect("mongodb://localhost:27017/todo-list")
     .then(()=>{
@@ -11,9 +30,7 @@ mongoose.connect("mongodb://localhost:27017/todo-list")
     })
     .catch((err)=>{
         console.log("Error connecting to MongoDB",err);
-    });
-
-
+});
 
 app.listen(port, () => {
     console.log(`Le serveur est lancé sur le port ${port}`);
